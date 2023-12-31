86d935078216546d438085362bce18a085c1c3d6
Sudip Biswas <sudipbiswas2142@gmail.com>
2023-12-31 17:17:03 +0530

    Header Component

---
+++ lib/constants.ts
@@ -0,0 +1,19 @@
+import { Instagram, Linkedin, Twitter } from "lucide-react";
+
+export const socialLinks = [
+  {
+    label: "Twitter",
+    route: "https://twitter.com/SudipB7_",
+    icon: Twitter,
+  },
+  {
+    label: "LinkedIn",
+    route: "https://linkedin.com/in/sudipb7",
+    icon: Linkedin,
+  },
+  {
+    label: "Instagram",
+    route: "https://instagram.com/sudipb7_",
+    icon: Instagram,
+  },
+];
