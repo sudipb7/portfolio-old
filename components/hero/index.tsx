dd15b930ff0d88f1416b589f765885c6e4df99e0
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-01-03 09:06:57 +0530

    Text Adjustment

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -6,9 +6,9 @@ import { Button } from "@/components/ui/button";
 export const Hero = () => {
   return (
     <section className="max-w-[700px] mx-auto mt-2 p-4">
-      <h3 className="font-semibold text-[16px] md:text-[17px] tracking-wide">
+      <h2 className="font-semibold text-lg md:text-xl tracking-wide">
         Passionate Full-Stack Web Developer
-      </h3>
+      </h2>
       <p className="mt-3 font-light text-[14px] md:text-[15px] text-secondary-foreground dark:text-zinc-100 tracking-wide mb-4">
         Just another passionate, skillful, and oh-so-creative web developer
         here. From crafting modern aesthetics to creating robust functionalities
--- components/skills/index.tsx
+++ components/skills/index.tsx
@@ -4,8 +4,8 @@ import { Badge } from "../badge";
 export const Skills = () => {
   return (
     <section className="max-w-[700px] mx-auto p-4 flex flex-col gap-3 mt-3">
-      <h3 className="text-[17px] font-semibold">Skillset</h3>
-      <div className="mt-3 flex flex-wrap items-center gap-2">
+      <h3 className="text-lg font-semibold">Skillset</h3>
+      <div className="flex flex-wrap items-center gap-2">
         {skills.map((skill, index) => (
           <Badge
             key={index}
