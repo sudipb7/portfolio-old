41906620ce5b64187d7f80eea0208b642981bfd1
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-01-20 19:08:54 +0530

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
+
 import './globals.css';
 
 const font = Poppins({
@@ -13,9 +15,65 @@ const font = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Sudip Biswas',
-  description:
-    'Just another passionate, skillful, and oh-so-creative web developer here.',
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: [
+    'Frontend Developer',
+    'UI Engineer',
+    'React Developer',
+    'JavaScript Developer',
+    'TypeScript Developer',
+    'Web Developer',
+    'Frontend Engineer',
+    'React Engineer',
+    'Full Stack Developer',
+    'Full Stack Engineer',
+    'Next.js Developer',
+    'Software Engineer',
+    'Software Developer',
+    'Frontend Development',
+    'React Development',
+  ],
+  authors: [
+    {
+      name: 'Sudip Biswas',
+      url: 'https://sudipbiswas.me',
+    },
+  ],
+  creator: 'Sudip Biswas',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: '@sudipb7_',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'Sudip Biswas',
+  url: 'https://sudipbiswas.me',
+  ogImage: 'https://sudipbiswas.me/ogImage.png',
+  description:
+    "I'm Sudip Biswas, a passionate, skillful, and oh-so-creative web developer here. From crafting modern aesthetics to creating robust functionalities using modern and scalable technologies.",
+  links: {
+    twitter: 'https://twitter.com/sudipb7_',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
