5f44d40c85a37d907560971b78d5e25ed3024a8f
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-01-27 13:23:03 +0530

    refactor:(package.json): Update npm packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,9 +78,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={cn(font.className, 'bg-[#FBFCFD] dark:bg-[#121212]')}>
