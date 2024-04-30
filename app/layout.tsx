82f3131dd21b3ece5be4f590433602322ecdae15
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-04-30 18:11:53 +0530

    webpush

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { siteConfig } from '@/lib/constants/site';
 import { cn } from '@/lib/utils';
 
 import './globals.css';
+import { WebPush } from '@/components/webpush';
 
 const font = Poppins({
   weight: ['300', '400', '500', '600'],
@@ -86,6 +87,7 @@ export default function RootLayout({
         <Analytics />
         <SpeedInsights />
         {children}
+        <WebPush />
       </body>
     </html>
   );
